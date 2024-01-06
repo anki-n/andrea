f1e03484b416f6fdff17b937dba62dcad97edaaf
anki-n <95865095+anki-n@users.noreply.github.com>
2024-01-07 00:18:56 +0100

    gallery

--- main.js
+++ main.js
@@ -115,6 +115,46 @@ lightGallery(document.getElementById('lightgallery'), {
 });
 
 
+$('#portfolio-filter span').click(function(){
+    
+  // Remove class 'active' from any <span> that is currently active 
+  $('#portfolio-filter .active').removeClass('active');
+    
+  // give this <span> that was clicked on a class of 'active' 
+  $(this).addClass('active');
+
+  // get the name of the category from this <span>, remove up to two spaces from the text and replace them with dashes, and make it lowercase 
+  var filterVal = $(this).text().replace(' ','-').replace(' ','-').toLowerCase();
+  
+  //console log
+  console.log ('filterVal has been set ' + filterVal);
+
+  // This is something new, it's an 'each' function which basically iterates through each element that matches the selector and applies the function one by one.
+  $('#filterable-gallery .gallery-item').each(function() {
+    
+    // If the filter value that they have clicked on is 'all' then remove the class of hidden from each gallery-item. 
+    if (filterVal == 'all') {
+      $(this).removeClass('hidden');
+      //console log
+      console.log ('removed class of hidden because set to view all');
+    }
+    
+    // If it's not all, then 
+    else {
+      if($(this).hasClass(filterVal)) {
+        $(this).removeClass('hidden'); // show those that have the filter class
+        //console log
+        console.log ('shows element with the filter');
+      }
+      else {
+        $(this).addClass('hidden'); // hide those that do not have the filter
+      }
+    }
+  });
+});
+
+
+
 // Slick Carousel
 $('.slider-container').slick({
 	autoplay: true, // Do we want it to autoplay? true or false
